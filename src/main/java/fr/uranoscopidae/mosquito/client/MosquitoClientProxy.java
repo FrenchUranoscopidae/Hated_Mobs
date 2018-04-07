8361175bb873db144841b5b49d3b1868b1f4660a
Admin <philippinemsm@laposte.net>
2018-04-07 14:48:08 +0200

    Net texture working !

--- src/main/java/fr/uranoscopidae/mosquito/client/MosquitoClientProxy.java
+++ src/main/java/fr/uranoscopidae/mosquito/client/MosquitoClientProxy.java
@@ -1,6 +1,15 @@
 package fr.uranoscopidae.mosquito.client;
 
+import fr.uranoscopidae.mosquito.ModMosquitos;
 import fr.uranoscopidae.mosquito.MosquitoCommonProxy;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.io.File;
 
@@ -10,12 +19,23 @@ public class MosquitoClientProxy extends MosquitoCommonProxy
     public void preInit(File configFile)
     {
         super.preInit(configFile);
-        System.out.println("pre init cote client");
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     @Override
     public void init()
     {
         super.init();
+    }
+
+    @SubscribeEvent
+    public void registerModels(ModelRegistryEvent event)
+    {
+        System.out.println("hello");
+        registerBlockModel(ModMosquitos.NET);
+    }
+
+    private void registerBlockModel(Block block) {
+        ModelLoader.setCustomModelResourceLocation(ItemBlock.getItemFromBlock(block), 0, new ModelResourceLocation(block.getRegistryName().toString(), "inventory"));
     }
 }
