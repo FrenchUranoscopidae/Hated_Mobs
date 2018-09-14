f209b9e797085cf7ad74a22f061b9a8a4e149a81
Admin <philippinemsm@laposte.net>
2018-09-14 08:51:00 +0200

    created TileEntityWaspNest class (empty)

---
+++ src/main/java/fr/uranoscopidae/hatedmobs/common/tileentities/TileEntityWaspNest.java
@@ -0,0 +1,13 @@
+package fr.uranoscopidae.hatedmobs.common.tileentities;
+
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ITickable;
+
+public class TileEntityWaspNest extends TileEntity implements ITickable
+{
+    @Override
+    public void update()
+    {
+
+    }
+}
